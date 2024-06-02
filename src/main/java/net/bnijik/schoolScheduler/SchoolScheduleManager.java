668801c4e2642a28d0687fe100ead44fcd02fb5a
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-06-01 21:33:32 -0400

    Name the app 'SchoolScheduleManager'

--- src/main/java/net/bnijik/schoolScheduler/SchoolScheduleManager.java
+++ src/main/java/net/bnijik/schoolScheduler/SchoolScheduleManager.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolScheduler {
+public class SchoolScheduleManager {
 
     public static void main(String[] args) {
-        SpringApplication.run(SchoolScheduler.class, args);
+        SpringApplication.run(SchoolScheduleManager.class, args);
     }
 }
