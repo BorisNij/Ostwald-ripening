a953caad79ea31ba10895d73c3f6332a230efeea
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-06-24 21:59:25 -0400

    remove student course and group components

--- src/main/java/net/bnijik/schoolScheduler/service/schedule/ScheduleService.java
+++
@@ -1,7 +0,0 @@
-package net.bnijik.schoolScheduler.service.schedule;
-
-import net.bnijik.schoolScheduler.dto.ScheduleDto;
-import net.bnijik.schoolScheduler.service.schoolAdmin.SchoolAdminService;
-
-public interface ScheduleService extends SchoolAdminService<ScheduleDto> {
-}
