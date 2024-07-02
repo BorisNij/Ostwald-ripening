8e3b0d6fc1f0c486a2aea9c37c6ca73fef877564
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-07-02 08:04:14 -0400

    feat: add GUID field to DTO enrich it with main instructor courses field

--- src/main/java/net/bnijik/schoolScheduler/dto/professor/ProfessorDto.java
+++ src/main/java/net/bnijik/schoolScheduler/dto/professor/ProfessorDto.java
@@ -1,12 +1,15 @@
-package net.bnijik.schoolScheduler.dto;
+package net.bnijik.schoolScheduler.dto.professor;
 
 import lombok.Builder;
+import net.bnijik.schoolScheduler.dto.CourseDto;
+import net.bnijik.schoolScheduler.dto.PagedDto;
 
 import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
 
 @Builder
-public record ProfessorDto(long professorId, String firstName, String lastName, Set<CourseDto> taughtCourses) {
+public record ProfessorDto(long professorId, UUID guid, String firstName, String lastName, Set<CourseDto> taughtCourses, Set<CourseDto> mainInstructorCourses) {
     public ProfessorDto {
         Objects.requireNonNull(firstName);
         Objects.requireNonNull(lastName);
@@ -16,9 +19,11 @@ public record ProfessorDto(long professorId, String firstName, String lastName, 
     public String toString() {
         return "{\n"
                 + "\t\"professorId\": " + professorId + ",\n"
+                + "\t\"guid\": " + guid + ",\n"
                 + "\t\"firstName\": \"" + firstName + "\",\n"
                 + "\t\"lastName\": \"" + lastName + "\",\n"
                 + "\t\"courses\": " + PagedDto.contentToString(taughtCourses) + "\n"
+                + "\t\"courses\": " + PagedDto.contentToString(mainInstructorCourses) + "\n"
                 + "}";
     }
 
--- src/main/java/net/bnijik/schoolScheduler/mapper/ProfessorMapper.java
+++ src/main/java/net/bnijik/schoolScheduler/mapper/ProfessorMapper.java
@@ -1,8 +1,10 @@
 package net.bnijik.schoolScheduler.mapper;
 
 
-import net.bnijik.schoolScheduler.dto.ProfessorDto;
+import net.bnijik.schoolScheduler.dto.professor.ProfessorCreateDto;
+import net.bnijik.schoolScheduler.dto.professor.ProfessorDto;
 import net.bnijik.schoolScheduler.entity.Professor;
+import net.bnijik.schoolScheduler.service.course.CourseService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -14,14 +16,20 @@ public abstract class ProfessorMapper implements SchoolModelMapper<Professor, Pr
     @Autowired
     protected CourseMapper courseMapper;
 
+    @Autowired
+    protected CourseService courseService;
+
     @Mapping(target = "professorId", expression = "java(model.professorId())")
+    @Mapping(target = "guid", expression = "java(model.guid())")
     @Mapping(target = "firstName", expression = "java(model.firstName())")
     @Mapping(target = "lastName", expression = "java(model.lastName())")
     @Mapping(target = "taughtCourses", expression = "java(courseMapper.modelsToDtos(model.taughtCourses()))")
+    @Mapping(target = "mainInstructorCourses", expression = "java(courseService.findAllMainInstructorCoursesForProfessor(model.professorId()))")
     @Override
     public abstract ProfessorDto modelToDto(Professor model);
 
     @Mapping(target = "professorId", expression = "java(dto.professorId())")
+    @Mapping(target = "guid", expression = "java(dto.guid())")
     @Mapping(target = "firstName", expression = "java(dto.firstName())")
     @Mapping(target = "lastName", expression = "java(dto.lastName())")
     @Mapping(target = "taughtCourses", expression = "java(courseMapper.dtosToModels(dto.taughtCourses()))")
