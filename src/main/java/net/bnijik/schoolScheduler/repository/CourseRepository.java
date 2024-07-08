e48d09b5cf30c5259de2613ba21b43149142365a
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-07-07 21:19:49 -0400

    fix: make findAllByCourseIdIn method return a set instead of slice

--- src/main/java/net/bnijik/schoolScheduler/repository/CourseRepository.java
+++ src/main/java/net/bnijik/schoolScheduler/repository/CourseRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -16,7 +15,7 @@ public interface CourseRepository extends SchoolRepository<Course> {
     Set<Course> findAllByMainInstructorProfessorId(long professorId);
     Slice<Course> findAllByStudentsStudentId(long studentId, Pageable pageable);
 
-    Slice<Course> findAllByCourseIdIn(List<Long> courseIds, Pageable pageable);
+    Set<Course> findAllByCourseIdIn(Set<Long> courseIds);
 
     @Transactional
     @Query("SELECT c FROM Course c LEFT JOIN FETCH c.students WHERE c.courseId = :courseId")
