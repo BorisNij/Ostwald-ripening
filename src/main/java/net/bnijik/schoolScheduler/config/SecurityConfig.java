3fb621c0c7996308813a538d1d38eda704f94d86
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-07-15 21:41:44 -0400

    feat: implement basic-auth with in-mem users

---
+++ src/main/java/net/bnijik/schoolScheduler/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package net.bnijik.schoolScheduler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/students/{guid}")
+                        .hasRole("STUDENT")
+                        .requestMatchers("/api/professors/{guid}")
+                        .hasRole("TEACHER")
+                        .requestMatchers(HttpMethod.GET, "/api/**")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/**")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/**")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/**")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        final User.UserBuilder pwdEncoderConfiguredUsrBuilder = User.builder().passwordEncoder(passwordEncoder::encode);
+        final UserDetails student = pwdEncoderConfiguredUsrBuilder.username("stud1")
+                .password("pwd1")
+                .roles("STUDENT")
+                .build();
+        final UserDetails professor = pwdEncoderConfiguredUsrBuilder.username("prof1")
+                .password("pwd2")
+                .roles("TEACHER")
+                .build();
+        final UserDetails admin = pwdEncoderConfiguredUsrBuilder.username("admin")
+                .password("pwd3")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(student, professor, admin);
+    }
+}
