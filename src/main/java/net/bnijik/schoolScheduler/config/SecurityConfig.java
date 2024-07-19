a40dfeeb967df718f4b8de6154f7f0191de7afde
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-07-18 23:08:37 -0400

    build: add Spring Security Test dependency

--- src/main/java/net/bnijik/schoolScheduler/config/SecurityConfig.java
+++ src/main/java/net/bnijik/schoolScheduler/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,17 +21,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/students/{guid}")
-                        .hasRole("STUDENT")
-                        .requestMatchers("/api/professors/{guid}")
-                        .hasRole("TEACHER")
-                        .requestMatchers(HttpMethod.GET, "/api/**")
-                        .hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/**")
-                        .hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/**")
-                        .hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/**")
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET ,"/api/students")
+                        .hasAnyRole("STUDENT", "ADMIN")
+                        .requestMatchers(HttpMethod.GET ,"/api/professors")
+                        .hasAnyRole("TEACHER", "ADMIN")
+                        .requestMatchers("/api/**")
                         .hasRole("ADMIN")
                         .anyRequest()
                         .authenticated())
@@ -41,22 +35,22 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         final User.UserBuilder pwdEncoderConfiguredUsrBuilder = User.builder().passwordEncoder(passwordEncoder::encode);
         final UserDetails student = pwdEncoderConfiguredUsrBuilder.username("stud1")
-                .password("pwd1")
+                .password(passwordEncoder.encode("pwd1"))
                 .roles("STUDENT")
                 .build();
         final UserDetails professor = pwdEncoderConfiguredUsrBuilder.username("prof1")
-                .password("pwd2")
+                .password(passwordEncoder.encode("pwd2"))
                 .roles("TEACHER")
                 .build();
         final UserDetails admin = pwdEncoderConfiguredUsrBuilder.username("admin")
-                .password("pwd3")
+                .password(passwordEncoder.encode("pwd3"))
                 .roles("ADMIN")
                 .build();
 
