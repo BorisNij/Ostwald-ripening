6fc3918f41b4ab950df63b591d6be72888470ebd
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-11-14 17:26:28 -0500

    fix: debug user password encryption and usage

--- src/main/java/net/bnijik/schoolScheduler/config/SecurityConfig.java
+++ src/main/java/net/bnijik/schoolScheduler/config/SecurityConfig.java
@@ -5,25 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET ,"/api/students")
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/api/students")
                         .hasAnyRole("STUDENT", "ADMIN")
-                        .requestMatchers(HttpMethod.GET ,"/api/professors")
+                        .requestMatchers(HttpMethod.GET, "/api/professors")
                         .hasAnyRole("TEACHER", "ADMIN")
                         .requestMatchers("/api/**")
                         .hasRole("ADMIN")
@@ -35,22 +33,22 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         final User.UserBuilder pwdEncoderConfiguredUsrBuilder = User.builder().passwordEncoder(passwordEncoder::encode);
         final UserDetails student = pwdEncoderConfiguredUsrBuilder.username("stud1")
-                .password(passwordEncoder.encode("pwd1"))
+                .password("pwd1")
                 .roles("STUDENT")
                 .build();
         final UserDetails professor = pwdEncoderConfiguredUsrBuilder.username("prof1")
-                .password(passwordEncoder.encode("pwd2"))
+                .password("pwd2")
                 .roles("TEACHER")
                 .build();
         final UserDetails admin = pwdEncoderConfiguredUsrBuilder.username("admin")
-                .password(passwordEncoder.encode("pwd3"))
+                .password("pwd3")
                 .roles("ADMIN")
                 .build();
 
